4aa2930a834260153fbd53807a1bec7ec2cda6d0
aniket <50738821+enigma-24@users.noreply.github.com>
2025-04-14 21:04:00 -0400

    refactored code for fibonacci sequence program

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,12 +1,10 @@
 const fibo = (n) => {
 	// n is the number of elements in the sequence to be displayed
-	if (n < 2) return [];
-
 	const fibSeq = [0, 1];
+	if (n <= 0) return [];
+	if (n === 1) return fibSeq[0];
 	if (n === 2) return fibSeq;
-	for (let i = 2; i < n; i++) {
-		fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
-	}
+	for (let i = 2; i < n; i++) fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
 	return fibSeq;
 };
 
