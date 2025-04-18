f1a3c25eb9e1c899d6e6d1c08a14f8f8ba7b6e66
aniket <50738821+enigma-24@users.noreply.github.com>
2025-04-18 18:30:44 -0400

    refactor code to find factorial of a number

--- src/factorial.js
+++ src/factorial.js
@@ -1,7 +1,6 @@
 const factorial = (n) => {
-	if (n < 0) return -1;
 	let factorial = 1;
-	if (n === 0 || n === 1) return factorial;
+	if (n < 1) return factorial;
 	for (let i = n; i > 1; i--) factorial *= i;
 	return factorial;
 };
