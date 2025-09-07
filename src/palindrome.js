02935f45806c15479680ce370cff39b3ff5b90ec
aniket <50738821+enigma-24@users.noreply.github.com>
2025-09-07 15:16:06 -0400

    implemented algorithm to check if string is palindrome

---
+++ src/palindrome.js
@@ -0,0 +1,15 @@
+const isPalindrome = (s) => {
+	let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if(s[left] !== s[right]) return false;
+
+        left += 1;
+        right -= 1;
+    }
+
+    return true;
+};
+
+module.exports = isPalindrome;
